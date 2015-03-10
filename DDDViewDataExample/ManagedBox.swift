453ffb2eddbd0c9d06bc542b6081f82d185d4e94
Christian Tietze <christian.tietze@gmail.com>
2015-03-10 11:33:09 +0100

    clean up Box wrapping

--- DDDViewDataExample/ManagedBox.swift
+++ DDDViewDataExample/ManagedBox.swift
@@ -51,20 +51,30 @@ public class ManagedBox: NSManagedObject, ManagedEntity {
     
     private var _box: Box?
     public lazy var box: Box = {
-        
-        let box = Box(boxId: self.boxId(), title: self.title)
-        let managedItems = self.items.allObjects as [ManagedItem]
-        let items = managedItems.map() { (item: ManagedItem) -> Item in
-            return item.item
-        }
-        box.items = items
-            
-        box.addObserver(self, forKeyPath: "title", options: .New, context: &boxContext)
-        box.addObserver(self, forKeyPath: "items", options: .New, context: &boxContext)
-        
+        let box = self.createBox()
+        self.observe(box)
         self._box = box
         return box
     }()
+    
+    func createBox() -> Box {
+        let box = Box(boxId: self.boxId(), title: self.title)
+        box.items = self.associatedItems()
+        
+        return box
+    }
+    
+    func associatedItems() -> [Item] {
+        let managedItems = self.items.allObjects as [ManagedItem]
+        return managedItems.map() { (item: ManagedItem) -> Item in
+            return item.item
+        }
+    }
+    
+    func observe(box: Box) {
+        box.addObserver(self, forKeyPath: "title", options: .New, context: &boxContext)
+        box.addObserver(self, forKeyPath: "items", options: .New, context: &boxContext)
+    }
     
     public override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
         
@@ -82,8 +92,12 @@ public class ManagedBox: NSManagedObject, ManagedEntity {
     }
     
     func mergeItems(items: [Item]) {
-        //TODO: create 2 delta arrays instead of iterating over all items twice
         let existingItems = self.mutableSetValueForKey("items")
+        removeMissingItems(items, from: existingItems)
+        addNewItems(items, to: existingItems)
+    }
+    
+    func removeMissingItems(items: [Item], from existingItems: NSMutableSet) {
         for item in existingItems {
             if let managedItem: ManagedItem = item as? ManagedItem {
                 if !contains(items, managedItem.item) {
@@ -91,7 +105,9 @@ public class ManagedBox: NSManagedObject, ManagedEntity {
                 }
             }
         }
-        
+    }
+    
+    func addNewItems(items: [Item], to existingItems: NSMutableSet) {
         for item in items {
             let itemIsInExistingItems = contains(existingItems, { (existingItem: AnyObject) -> Bool in
                 let managedItem = existingItem as ManagedItem
@@ -106,9 +122,12 @@ public class ManagedBox: NSManagedObject, ManagedEntity {
     
     deinit {
         if let box = _box {
-            box.removeObserver(self, forKeyPath: "title")
-            box.removeObserver(self, forKeyPath: "items")
+            unobserve(box)
         }
     }
 
+    func unobserve(box: Box) {
+        box.removeObserver(self, forKeyPath: "title")
+        box.removeObserver(self, forKeyPath: "items")
+    }
 }
