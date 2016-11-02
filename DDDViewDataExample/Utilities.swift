c62393e4a9906a62f56ae3847ad27fcde395ecf1
Christian Tietze <christian.tietze@gmail.com>
2016-11-02 09:01:30 +0100

    clean up delay() for Swift 3

--- DDDViewDataExample/Utilities.swift
+++ DDDViewDataExample/Utilities.swift
@@ -1,9 +1,10 @@
 import Foundation
 import CoreData
 
-func delay(_ delay: Double, closure: @escaping () -> ()) {
+func delay(_ delay: Double, closure: @escaping () -> Void) {
     DispatchQueue.main.asyncAfter(
-        deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
+        deadline: DispatchTime.now() + .milliseconds(Int(delay * 1000)),
+        execute: closure)
 }
 
 func dispatch_async_main(_ closure: @escaping () -> ()) {
